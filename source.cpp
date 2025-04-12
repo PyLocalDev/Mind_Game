f8fc09dde2dee09c880b8bd85b6c26edb0074c95
PYLOCALDEV <brillyed23@gmail.com>
2025-04-12 09:01:32 +0000

    Finished UI Things, etc.
    This commit finishes UI Things, looping choice and formatting.

--- source.cpp
+++ source.cpp
@@ -3,12 +3,36 @@
 class Game {
   public:
     void menu() {
-      std::cout << std::endl;
-      std::cout << "MIND GAME!   Made by PyLocalDev!" << std::endl;
-      std::cout << "This game is simple. Solve The Puzles!" << std::endl;
-      std::cout << std::endl;
-      std::cout << "Please choose a option :" << std::endl;
-      std::cout << "   (S)tart the Game" << std::endl << "   (D)iffeculty" << std::endl << "   (E)xit" << std::endl;
+      for(int i = -1; i != 0; i--) {
+        std::cout << std::endl;
+        std::cout << "MIND GAME!   Made by PyLocalDev!" << std::endl;
+        std::cout << "This game is simple. Solve The Puzles!" << std::endl;
+        std::cout << std::endl;
+        std::cout << "Please choose a option :" << std::endl;
+        std::cout << "   (S)tart the Game" << std::endl << "   (D)iffeculty" << std::endl << "   (E)xit" << std::endl;
+
+        char opt;
+        std::cout << "Please enter the option and press enter: ";
+        std::cin >> opt;
+
+        switch(opt) {
+          case 's':
+            std::cout << "start" << std::endl;
+            break;
+
+          case 'd':
+            std::cout << "difficultity" << std::endl;
+            break;
+
+          case 'e':
+            std::cout << "Exit" << std::endl;
+            break;
+
+          default:
+            std::cout << "please the option given above." << std::endl;
+            break;
+        }
+      }
     }
 };
 
